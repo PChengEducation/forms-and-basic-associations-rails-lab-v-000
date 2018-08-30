c0dbb893918f6f7dd47cdd28a8668bb726d24859
Pan Cheng <pchengeducation@hotmail.com>
2018-08-30 01:42:01 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,31 +1,38 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-
-  #custom setter for artist name
+  
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+  
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
-
-  #reader for artist name
-  def artist_name
-    self.artist.name if self.artist
+  
+  def artist_name 
+    try(:artist).try(:name)
+    #binding.pry
+    #artist = Artist.find_or_create_by(name: name)
+    #self.artist = artist 
   end
-
-  #custom setter for note contents from array params
+  
+  def genre_name 
+    try(:genre).try(:name)
+  end
+  
   def note_contents=(notes)
-    notes.each do |note|
-      if note.strip != ""
-        content = self.notes.build(content: note)
-        content.save
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
       end
     end
   end
-
-  #reader for note contents
+  
   def note_contents
-    self.notes.collect {|note| note.content }
+    self.notes.map(&:content)
   end
-
-end
+end 
